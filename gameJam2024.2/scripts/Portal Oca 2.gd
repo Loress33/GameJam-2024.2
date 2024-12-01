e0e23d491a1d1901b3b16087291171725db95975
Loress33 <lorenzoa@al.insper.edu.br>
2024-12-01 11:53:52 -0300

    Agora sim fiz as ocas e a vila

---
+++ gameJam2024.2/scripts/Portal Oca 2.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var player_in_range = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player_in_range and Input.is_action_just_pressed("interact"):
+		get_tree().change_scene_to_file("res://scenes/vila.tscn")
+
+func _on_area_2d_area_entered(body: CharacterBody2D) -> void:
+	if body.is_in_group("player"):
+		player_in_range = true
+
+func _on_area_2d_area_exited(body: CharacterBody2D) -> void:
+	if body.is_in_group("player"):
+		player_in_range = false
